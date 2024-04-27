8651bcd9dae47fa2b59678e53ad064cf1408ad7f
SamBishop06 <samuelbishop06@yahoo.com>
2024-04-27 00:17:39 -0400

    index;user;though.js

---
+++ routes/api-routes/thoughtRoutes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
+} = require('../../controllers/thoughtController');
+
+// Set up GET all and POST requests for /api/thoughts
+router.route('/').get(getAllThoughts).post(createThought);
+
+// Set up GET one, PUT, and DELETE requests for /api/thoughts/:id
+router.route('/:thoughtId').get(getThoughtById).put(updateThought).delete(deleteThought);
+
+router.route('/:thoughtId/reactions') .post(addReaction).delete(deleteReaction);
+
+module.exports = router;
